152a73f69ba29615e94f37f41ce536624621f2d1
Anna Luczak <an.archeo@gmail.com>
2023-07-10 22:54:27 +0100

    created Company POJO

---
+++ src/main/java/com/project/itservicedesk/models/Company.java
@@ -0,0 +1,28 @@
+package com.project.itservicedesk.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@ToString(of = "companyName")
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Company_name", nullable = false, unique = true)
+    private String companyName;
+
+    @OneToMany(mappedBy = "company")
+    private Set<Project> projects = new HashSet<>();
+}
