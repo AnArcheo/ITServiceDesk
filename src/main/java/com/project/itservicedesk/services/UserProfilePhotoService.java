1e39580d8ec678b28f8ea8cec45317c997879161
Anna Luczak <an.archeo@gmail.com>
2023-07-11 22:38:34 +0100

    created UserProfilePhotoService

---
+++ src/main/java/com/project/itservicedesk/services/UserProfilePhotoService.java
@@ -0,0 +1,37 @@
+package com.project.itservicedesk.services;
+
+import com.project.itservicedesk.models.User;
+import com.project.itservicedesk.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@AllArgsConstructor
+public class UserProfilePhotoService implements ImageService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public void saveUserProfilePhoto(Long userId, MultipartFile file) {
+        User user = userRepository.findById(userId).get();
+
+        try{
+            Byte[] bytes = new Byte[file.getBytes().length];
+            int i = 0;
+            for (byte b : file.getBytes()){
+                bytes[i++] = b;
+            }
+            user.setProfilePhoto(bytes);
+            userRepository.save(user);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
