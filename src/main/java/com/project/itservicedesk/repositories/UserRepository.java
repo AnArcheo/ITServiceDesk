350993aebc302b5f4cdc6f9ae7caf696f3c8affd
Anna Luczak <an.archeo@gmail.com>
2023-07-11 21:52:51 +0100

    created UserRepository

---
+++ src/main/java/com/project/itservicedesk/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.project.itservicedesk.repositories;
+
+import com.project.itservicedesk.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    //    Page<User> searchRepository(String keyword, Pageable pageable);
+    List<User> findAllUsersByUsername(String username);
+    User findByUsername(String username);
+    @Query(value = "SELECT u FROM User u WHERE u.email=?1")
+    public User findByEmail(String email);
+
+}
