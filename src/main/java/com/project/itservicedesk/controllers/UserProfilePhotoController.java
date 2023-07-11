d862a8bc806db7dfbca41d3aaa1654edc5d7b7a7
Anna Luczak <an.archeo@gmail.com>
2023-07-11 22:38:16 +0100

    created UserProfilePhotoController

---
+++ src/main/java/com/project/itservicedesk/controllers/UserProfilePhotoController.java
@@ -0,0 +1,23 @@
+package com.project.itservicedesk.controllers;
+
+import com.project.itservicedesk.services.UserProfilePhotoService;
+import com.project.itservicedesk.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@AllArgsConstructor
+public class UserProfilePhotoController {
+
+//    private final UserService userService;
+//    private final UserProfilePhotoService userProfilePhotoService;
+//
+//    @GetMapping("/users/{id}/photo")
+//    public String showUploadPhotoForm(@PathVariable("id") Long id, Model model){
+//
+//    }
+
+}
