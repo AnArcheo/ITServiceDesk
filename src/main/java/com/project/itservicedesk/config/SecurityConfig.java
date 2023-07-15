ead3671eb1f4ce2bc64e1df20ac983ad13694f62
Anna Luczak <an.archeo@gmail.com>
2023-07-15 23:15:37 +0100

    amended SecurityConfig

--- src/main/java/com/project/itservicedesk/config/SecurityConfig.java
+++ src/main/java/com/project/itservicedesk/config/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.project.itservicedesk.config;
 
-import com.project.itservicedesk.services.UserSecurityDetailsService;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,50 +25,66 @@ import org.springframework.security.web.SecurityFilterChain;
 @AllArgsConstructor
 public class SecurityConfig {
 
-    private final UserSecurityDetailsService userSecurityDetailsService;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//            HttpSecurity httpSecurity,
-//            PasswordEncoder passwordEncoder,
-//            UserDetailsService userDetailsService
-//    ) throws Exception{
-//        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(passwordEncoder)
-//                .and() // <--- deprecated
-//                .build();
-//
-//    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService
+    ){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
+    }
 
     @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity httpSecurity,
+            AuthenticationProvider authenticationProvider
+    ) throws Exception{
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(authenticationProvider)
+                .build();
+
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "true", matchIfMissing = true)
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        // Configure AuthenticationManagerBuilder
-        AuthenticationManagerBuilder authenticationManagerBuilder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userSecurityDetailsService)
-                .passwordEncoder(passwordEncoder());
 
-        // Get AuthenticationManager
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-        httpSecurity.authorizeHttpRequests()
-                .requestMatchers("/login", "/error", "/images/oh_no.png").permitAll()
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)// (csrf) -> csrf.disable() or provide method reference
+                .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/","/login", "/error", "/images/avatars/**").permitAll()
                 .requestMatchers(HttpMethod.DELETE).hasAuthority("ADMIN")
-                .requestMatchers("/employees/**", "/images/**").hasAnyAuthority("USER", "ADMIN")
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .logout()
+                .requestMatchers("/users/**","/tasks/**","/bugs/**", "/images/**")
+                                .hasAnyAuthority("USER", "ADMIN") // TODO: add other endpoints and user roles
+                                .requestMatchers(HttpMethod.DELETE).hasAuthority("ADMIN") // TODO: add other user roles
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll) // form -> form.permitAll() or provide method reference
+                .logout(logout -> logout
                 .logoutSuccessUrl("/login")
                 .invalidateHttpSession(true)
                 .deleteCookies("JSESSIONID")
-                .permitAll();
-        return httpSecurity.build();
+                .permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "false")
+    SecurityFilterChain securityDisabled(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable) // or provide method reference
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest()
+                        .permitAll()
+                )
+                .build();
     }
 }
