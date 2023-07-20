9c4b3ebd2144e87b471488a4fc65257af2714e83
Anna Luczak <an.archeo@gmail.com>
2023-07-20 22:36:16 +0100

    amended UserRegistrationDTO

--- src/main/java/com/project/itservicedesk/dto/UserRegistrationDTO.java
+++ src/main/java/com/project/itservicedesk/dto/UserRegistrationDTO.java
@@ -1,13 +1,44 @@
 package com.project.itservicedesk.dto;
 
 import com.project.itservicedesk.models.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public record UserRegistrationDTO(
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 30)
         String username,
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 256)
         String password,
+
+        @NotBlank
+        @NotEmpty
+        @Email
+        @Size(max = 45)
         String email,
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 20)
         String firstname,
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 20)
         String lastname,
+        @NotBlank
+        @NotEmpty
+        @Size(max = 10)
+        @Enumerated(value = EnumType.STRING)
         Gender gender
 ) {
 }
