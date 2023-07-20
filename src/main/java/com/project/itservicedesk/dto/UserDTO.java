92fa762fe7a241da8ad4aa2fe3b082afcf66b130
Anna Luczak <an.archeo@gmail.com>
2023-07-20 22:35:35 +0100

    amended UserDTO

--- src/main/java/com/project/itservicedesk/dto/UserDTO.java
+++ src/main/java/com/project/itservicedesk/dto/UserDTO.java
@@ -2,14 +2,34 @@ package com.project.itservicedesk.dto;
 
 import com.project.itservicedesk.models.Project;
 import com.project.itservicedesk.models.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record UserDTO(
         Long id,
+        @NotBlank
+        @NotEmpty
+        @Size(max = 30)
         String username,
+
+        @NotBlank
+        @NotEmpty
+        @Email
+        @Size(max = 45)
         String email,
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 20)
         String firstname,
+
+        @NotBlank
+        @NotEmpty
+        @Size(max = 20)
         String lastname,
         List<String> roles,
         List<String> projects
