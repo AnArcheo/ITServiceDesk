9d49268d43319f0b05c616afc39aefc61caaf636
Anna Luczak <an.archeo@gmail.com>
2023-07-26 23:38:57 +0100

    created BugController

---
+++ src/main/java/com/project/itservicedesk/controllers/BugController.java
@@ -0,0 +1,11 @@
+package com.project.itservicedesk.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/bugs")
+public class BugController {
+}
