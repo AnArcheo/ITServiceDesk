49032c57b03cf7eb2a13b499f139b61435efaa5d
Anna Luczak <an.archeo@gmail.com>
2023-07-26 23:46:44 +0100

    created TaskComment

---
+++ src/main/java/com/project/itservicedesk/models/TaskComment.java
@@ -0,0 +1,41 @@
+package com.project.itservicedesk.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@ToString(of = {"author", "commentContent"})
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="task_comments")
+public class TaskComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="created_date", nullable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @Column(name = "author")
+    private String author;
+
+    @Column(name="comment_content", nullable = false)
+    @NotEmpty(message = "Comment cannot be empty!")
+    @Size(min = 2, max = 255, message = "Comment must have minimum 2 and maximum 255 characters long")
+    private String commentContent;
+}
